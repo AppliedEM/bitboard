7e92921437138a45ba2f5f5a3c659a37fc6b5b81
Michael van Dyk <michael@biochemist.me>
2018-03-03 19:05:11 -0500

    updated flask

--- complete/frontend_flask.py
+++ complete/frontend_flask.py
@@ -1,15 +1,37 @@
 from flask import Flask
 from flask import request
 app = Flask(__name__)
+import transactions
+import btccore
 
+use_testnet = True
 @app.route('/')
 def index():
+	print(request.args)
 	return app.send_static_file('index.html')
 
 @app.route("/hello/")
 def hello_world():
 	return "Hello, world!"
 
+@app.route('/mymoney')
+def get_balance():
+	global use_testnet
+	utxos = transactions.grab_utxos(str(request.args['account_id']),use_testnet)
+	balance = transactions.sum_utxos(utxos)
+	return str(balance)
+
+@app.route('/sendmoney')
+def send_money():
+	global use_testnet
+	#print(request.args)
+	account_id = str(request.args['account_id'])
+	target = str(request.args['target_addr'])
+	price = int(request.args['price'])
+	fee = int(request.args['fee'])
+	
+	return str(push_attempt)
+	
 @app.route("/testpost/",methods=["GET","POST"])
 def test_post():
 	if request.method=="GET":
