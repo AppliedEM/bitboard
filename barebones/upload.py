a50edc176a5d919a67c56400c1278fa2bf84a377
Ryan Whitney <user7926@gmail.com>
2018-03-27 15:08:55 -0400

    fixed esptool upload script maybe?

---
+++ barebones/upload.py
@@ -0,0 +1,26 @@
+import serial.tools.list_ports
+import esptool
+import subprocess
+
+def guessarduport():
+    ports = list(serial.tools.list_ports.comports())
+    for p in ports:
+        if('USB' in p.description and ('SERIAL' in p.description or 'Serial' in p.description)):
+            return p.device
+
+def listports():
+    ports = list(serial.tools.list_ports.comports())
+    for p in ports:
+        p
+
+p = guessarduport()
+print("Guessed arduino port. Is this correct? (y/n): " + p)
+resp = input()
+if resp == 'n':
+    print("-------List of ports---------")
+    listports()
+    print("-----------------------------")
+    print("Please locate port manually and input: ")
+    p = input()
+
+subprocess.call(['python', 'esptool.py', '--port', p, 'write_flash', '0x00000', 'arduclient.ino.nodemcu.bin'])
